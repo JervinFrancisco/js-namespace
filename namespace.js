8068a93d20125190bf56e8c47e30d310dab50e2b
fran0436 <fran0436@algonquinlive.com>
2017-10-15 19:25:38 -0400

    namespace
    namespace

---
+++ namespace.js
@@ -0,0 +1,26 @@
+var FRAN0436 = {
+    init: function () {
+        var box = document.createElement('div'),
+        boxes = document.getElementById('boxes');
+        box.textContent = 'FRAN0436';
+        box.classList.add('box');
+        boxes.appendChild(box);
+        // EventListners
+        box.addEventListener('click', MKlick);
+        box.addEventListener('mouseover', MOver);
+        box.addEventListener('mouseout', MOut);
+
+        function MKlick(ev) {
+            ev.currentTarget.style.borderColor = "#FFFF00";
+            ev.currentTarget.style.backgroundColor = "#800000";
+        }
+
+        function MOver(ev) {
+            ev.currentTarget.classList.toggle('highlight');
+        }
+
+        function MOut(ev) {
+            ev.currentTarget.classList.toggle('highlight');
+        }
+    }
+};
