37772134e539105b7bbb797292ef6692bd3739b7
fran0436 <fran0436@algonquinlive.com>
2017-10-15 20:33:36 -0400

    font 3

--- namespace.js
+++ namespace.js
@@ -1,20 +1,20 @@
 var FRAN0436 = {
 
     init: function () {
-        var box = document.createElement('div'),
+        var clevland = document.createElement('div'),
         boxes = document.getElementById('boxes');
-        box.textContent = 'FRAN0436';
-        box.classList.add('box');
+        clevland.textContent = 'FRAN0436';
+        clevland.classList.add('box');
         boxes.appendChild(box);
         // EventListners
-        box.addEventListener('click', MKlick);
-        box.addEventListener('mouseover', MOver);
-        box.addEventListener('mouseout', MOut);
+        clevland.addEventListener('click', MKlick);
+        clevland.addEventListener('mouseover', MOver);
+        clevland.addEventListener('mouseout', MOut);
 
         function MKlick(ev) {
             ev.currentTarget.style.borderColor = "#FFFF00";
             ev.currentTarget.style.backgroundColor = "#800000";
-            ev.currentTarget.style.font = "bold";
+            ev.currentTarget.style.fontWeight = "bold";
         }
 
         function MOver(ev) {
